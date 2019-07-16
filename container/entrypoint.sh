9d7855099f67a111f82d54d637a3ff58c8765489
Aaron <1496339+promythyus@users.noreply.github.com>
2019-07-16 12:02:12 +1000

    Change permissions set on /etc/bind for #23
    Changes the entrypoint script to give write permissions to `named` within `/etc/bind`. This allows `named` to create a zone journal file so that dynamic updates can be successfully applied.

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -3,6 +3,6 @@ OPTIONS=$@
 chown -R root:named /etc/bind /var/run/named
 chown -R named:named /var/cache/bind
 chmod -R 770 /var/cache/bind /var/run/named
-chmod -R 750 /etc/bind
+chmod -R 770 /etc/bind
 # Run in foreground and log to STDERR (console):
 exec /usr/sbin/named -c /etc/bind/named.conf -g -u named $OPTIONS
